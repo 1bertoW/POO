8680b958668dffd47b5b88826fe88a599897ce13
1berto <144922529+1bertoW@users.noreply.github.com>
2025-03-24 08:02:22 -0600

    Add files via upload

---
+++ Q01/src/br/edu/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int N1 = 10;
+		int N2 = 15;
+		
+		int soma = N1 + N2;
+		
+		System.out.println("o primeiro numero é::"  +  N1);
+		System.out.println("o segundo numero é::"  +  N2);
+		System.out.println("o resultado da soma é:"  +  soma);
+
+	}
+
+}
